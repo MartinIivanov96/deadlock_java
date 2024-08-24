a8f3c2be773448a57f6c357863377ba782850d76
MartinIivanov96 <martiniivanov96@gmail.com>
2024-08-24 03:20:39 +0200

    Deadlock v1.0. Find and reproduce the daedlock

---
+++ src/unidue/de/Cell.java
@@ -0,0 +1,25 @@
+package unidue.de;
+
+ public class Cell {
+    private long value;
+
+    public Cell(long value) {
+        this.value = value;
+    }
+
+    public synchronized long getValue() {
+        return value;
+    }
+
+    public synchronized void setValue(long value) {
+        this.value = value;
+    }
+
+    public synchronized void swapValue(Cell other) {
+        long temp = getValue();
+        long newValue = other.getValue();
+        setValue(newValue);
+        other.setValue(temp);
+    }
+ }
+
